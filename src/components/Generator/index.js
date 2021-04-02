bc4d19723d238dd8a56f64c6ad77553d15648c69
Yuliya Kandratsyeva <ykandratsyeva@gmail.com>
2021-04-02 14:32:42 -0400

    adding ability for the compoent to fetch and display API response data

--- src/components/Generator/index.js
+++ src/components/Generator/index.js
@@ -1,34 +1,71 @@
 // import axios from 'axios';
-import React, { useEffect } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { Container } from "react-bootstrap";
+import "../../styles/Generator.css";
 
 export default function Generator() {
+  const [state, setState] = useState([]);
+
   useEffect(() => {
     function activateEventListener(event) {
       const { code } = event;
 
-      console.log(event);
-
       if (code === "Space") {
-        console.log("space bar");
-        // call space bar handler here
+        fetchData();
       }
     }
 
     document.addEventListener("keydown", activateEventListener);
 
+    async function fetchData() {
+      await axios
+        .get("http://www.boredapi.com/api/activity")
+        .then((response) => setState(response.data))
+        .catch((error) =>
+          console.error(
+            "Something went wrong when fetching the data from the API ",
+            error
+          )
+        );
+    }
+
     return () => {
-      //cleaning up
+      //cleaning up to prevent memory leaks
       document.removeEventListener("keydown", activateEventListener);
     };
   }, []);
 
   return (
     <>
-      <Container>Task willl be rendered here</Container>
+      <Container className="wrapper" fluid>
+        <Container id="random-activity">
+          <p>You can {state.activity}</p>
+          <p>
+            with{" "}
+            {state.participants === 1
+              ? "1 person "
+              : `${state.participants} people `}
+          </p>
+          <p>{state.price === 0 ? "and it's free!" : null}</p>
+          {state.link ? (
+            <p>
+              Here's the <a href={state.link}>link</a>
+            </p>
+          ) : null}
+        </Container>
+      </Container>
     </>
   );
 }
 
-//     const response = await axios.get("http://www.boredapi.com/api/activity");
-//     const data = await response.data;
+// data format
+// {
+//   activity: "Clean out your closet and donate the clothes you've outgrown",
+//   type: "charity",
+//   participants : 1,
+//   price: 0,
+//   link: "",
+//   key: "9026787",
+//   accessibility: 0.1
+// }
