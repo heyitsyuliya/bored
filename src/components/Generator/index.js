ee8d3c0818e0cceae14089d10b293731cd9d32ef
Yuliya Kandratsyeva <ykandratsyeva@gmail.com>
2021-04-02 13:54:58 -0400

    refactoring main component to use hooks

--- src/components/Generator/index.js
+++ src/components/Generator/index.js
@@ -1,11 +1,34 @@
-import React from "react";
-import { Button, Container } from "react-bootstrap";
-import { makeRequest } from "./utils";
+// import axios from 'axios';
+import React, { useEffect } from "react";
+import { Container } from "react-bootstrap";
 
-export default function index() {
+export default function Generator() {
+  useEffect(() => {
+    function activateEventListener(event) {
+      const { code } = event;
+
+      console.log(event);
+
+      if (code === "Space") {
+        console.log("space bar");
+        // call space bar handler here
+      }
+    }
+
+    document.addEventListener("keydown", activateEventListener);
+
+    return () => {
+      //cleaning up
+      document.removeEventListener("keydown", activateEventListener);
+    };
+  }, []);
+
   return (
-    <Container>
-      <Button onClick={makeRequest}>Tell me what to do</Button>
-    </Container>
+    <>
+      <Container>Task willl be rendered here</Container>
+    </>
   );
 }
+
+//     const response = await axios.get("http://www.boredapi.com/api/activity");
+//     const data = await response.data;
